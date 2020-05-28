342f2a043645bc15c38ac122789bffb53b6782d1
angel-torres <alas4224@gmail.com>
2020-05-27 23:56:12 -0500

    added some session configuration in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,18 @@
 require('dotenv').config();
 import express from 'express';
 import cors from 'cors';
+import session from 'express-session';
 import authRoute from './routes/auth';
 
 const app = express();
+
+app.set('trust proxy', 1) // trust first proxy
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}))
 
 app.use(cors())
 app.use('/auth', authRoute)
