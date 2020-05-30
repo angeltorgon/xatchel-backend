927a0faa65f72dfbd56344041cb5c79eddb8a01e
angel-torres <alas4224@gmail.com>
2020-05-30 00:15:41 -0500

    added generatePassword functionality in authUtils.ts

--- src/lib/authUtils.ts
+++ src/lib/authUtils.ts
@@ -1,7 +1,16 @@
 import crypto from 'crypto';
 
+interface validatePasswordInterface {
+    salt: string;
+    hash: string;
+}
 
-export function validatePassword(){}
+export function validatePassword(password: string): validatePasswordInterface {
+    const salt = crypto.randomBytes(32).toString('hex');
+    const hash = crypto.pbkdf2Sync(password, salt, 10000, 64, 'sha512').toString('hex');
+
+    return { salt, hash }
+}
 
 export function generatePassword(){}
 
