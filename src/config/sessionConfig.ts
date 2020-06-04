29a66dd6c9a3bb314772053de2edb1a0ba34f5db
angel-torres <alas4224@gmail.com>
2020-06-04 12:24:13 -0500

    removed express-session and @types/express-session from dependencies because it caused errors

--- src/config/sessionConfig.ts
+++ src/config/sessionConfig.ts
@@ -1,12 +1,12 @@
-import session from 'express-session';
-import connection from './connectionConfig';
-const MongoStore = require('connect-mongo')(session);
+// import session from 'express-session';
+// import connection from './connectionConfig';
+// const MongoStore = require('connect-mongo')(session);
 
 const sessionConfig = {
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true,
-  store: new MongoStore({mongooseConnection: connection, collection: 'sessions'}),
+  // store: new MongoStore({mongooseConnection: connection, collection: 'sessions'}),
   cookie: { maxAge: 6000 }
 };
 
