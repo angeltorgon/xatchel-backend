16b6519a06bd351dc5d16545c76c252aa2e263f9
angel-torres <alas4224@gmail.com>
2020-06-04 12:27:25 -0500

    cleaned up and removed extra comments

--- src/config/sessionConfig.ts
+++ src/config/sessionConfig.ts
@@ -1,12 +1,7 @@
-// import session from 'express-session';
-// import connection from './connectionConfig';
-// const MongoStore = require('connect-mongo')(session);
-
 const sessionConfig = {
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true,
-  // store: new MongoStore({mongooseConnection: connection, collection: 'sessions'}),
   cookie: { maxAge: 6000 }
 };
 
