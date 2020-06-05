226eb93608658bc626b3530310ccb34d4aa59f27
angel-torres <alas4224@gmail.com>
2020-06-04 21:48:48 -0500

    added check for envirnment PORT variable

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import app from './server';
 
-const PORT = 3500
+const PORT = process.env.PORT || 3500
 
 const server = app.listen(PORT, () => {
     console.log(`Port is running on port ${PORT}`);
